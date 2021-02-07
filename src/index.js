7df70fd850e24fd4af6fd7ebe24620cac41fa877
Manuel Marin Duque <manuel.duque5.2@gmail.com>
2021-02-07 16:43:56 -0500

    Las rutas para gas_station ya es global.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.json());
 
 // Routes
-app.use(require('./routes/gas_station'));
+app.use('/station', require('./routes/gas_station'));
 
 // Starting the server
 app.listen(app.get('port'), () => {
--- src/routes/gas_station.js
+++ src/routes/gas_station.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const mysqlConnection = require('../database');
 const jwt = require('jsonwebtoken');
 
-router.get('/get_data', (req, res) => {
+router.get('/', (req, res) => {
     mysqlConnection.query('SELECT * FROM gas_station', (error, rows) => {
         error ? res.status(500).json({message: error.message}) : res.status(200).json({token: jwt.sign({gas_stations: rows}, '3ywg&hsnxu43o9+iaz&sdtr')})
     });
