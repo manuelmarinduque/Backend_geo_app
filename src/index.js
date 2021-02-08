5c8cd586a2c8fa6562fce0c48bf8b3bb9bbbf88e
Stiven <stiven.pinzon@correounivalle.edu.co>
2021-02-08 01:36:37 -0500

    Add http verbs in station

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-const express = require('express');
+const express = require("express");
+const station = require("./routes/gas_station");
 const app = express();
 
 // Settings
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
 // Middlewares
 app.use(express.json());
 
 // Routes
-app.use('/station', require('./routes/gas_station'));
+app.use("/station", station);
 
 // Starting the server
-app.listen(app.get('port'), () => {
-    console.log(`Server on port ${app.get('port')}`)
+app.listen(app.get("port"), () => {
+  console.log(`Server on port ${app.get("port")}`);
 });
