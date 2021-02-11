ea82bc75e854f79f81a54db0daa858bb07c5cea4
Manuel Marin Duque <manuel.duque5.2@gmail.com>
2021-02-10 21:37:32 -0500

    Se protegen las debidas rutas de user las cuales necesitan un jwt token de acceso.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,84 +3,89 @@ const router = express.Router();
 const mysqlConnection = require('../database');
 const { hashSync, genSaltSync, compareSync } = require("bcrypt");
 const jwt = require('jsonwebtoken');
+const token_required = require('../functions/token_required_function')
 
 router.post('/create_user', (req, res) => {
     const salt = genSaltSync(10);
     req.body.password = hashSync(req.body.password, salt);
-    mysqlConnection.query('INSERT INTO usuario SET ?', req.body, (error, row) => {
-        error ? res.status(500).json({message: error.message}) : res.status(200).json({data: row})
+    mysqlConnection.query('INSERT INTO usuario SET ?', req.body, (error) => {
+        error ? res.status(500).json({ message: error.sqlMessage }) : res.status(200).json({ message: "User created successfully." })
     })
 })
 
 router.get('/get_users', (req, res) => {
     mysqlConnection.query('SELECT * FROM usuario WHERE is_active=1', (error, row) => {
-        error ? res.status(500).json({message: error.message}) : res.status(200).json({data: row})
+        error ? res.status(500).json({ message: error.message }) : res.status(200).json({ data: row })
     })
 })
 
 router.get('/get_user', token_required, (req, res) => {
     jwt.verify(req.token, "3ywg&hsnxu43o9+iaz&sdtr", (error, data) => {
-        error? res.status(403).json({message: 'Invalid token'}) : res.status(200).json({data: data.row[0]})
+        error ? res.status(403).json({ message: 'Invalid token' }) : res.status(200).json({ data: data.row[0] })
     })
 })
 
-router.put('/update_user/:doc_number', (req, res) => {
-    const { doc_number, doc_type, full_name, genre, nacionality, address, phone } = req.body
-    mysqlConnection.query('UPDATE usuario SET doc_number=?, doc_type=?, full_name=?, genre=?, nacionality=?, address=?, phone=? WHERE doc_number=? AND is_active=1',
-    [doc_number, doc_type, full_name, genre, nacionality, address, phone, req.params.doc_number],
-    (error, row) => {
-        error ? res.status(500).json({message: error.message}) : res.status(200).json({data: row})
-    })
-})
-
-router.post('/change_password/:doc_number', (req, res) => {
-    mysqlConnection.query(`SELECT * FROM usuario WHERE doc_number=${req.params.doc_number} AND is_active=1`, (error, row) => {
+router.put('/update_user', token_required, (req, res) => {
+    jwt.verify(req.token, "3ywg&hsnxu43o9+iaz&sdtr", (error, data) => {
         if (error) {
-            res.status(500).json({message: error.message})      
+            res.status(400).json({ message: error.message })
         } else {
-            if (compareSync(req.body.new_password, row[0].password)) {
-                res.status(400).json({message: 'La nueva contraseña es igual a la anterior'})
-            } else {
-                const salt = genSaltSync(10);
-                new_password = hashSync(req.body.new_password, salt);
-                mysqlConnection.query('UPDATE usuario SET password=? WHERE doc_number=?', [new_password, req.params.doc_number],
-                (error, row) => {
-                    error ? res.status(500).json({message: error.message}) : res.status(200).json({data: row})
+            const { doc_number, doc_type, full_name, genre, nacionality, address, phone } = req.body
+            mysqlConnection.query('UPDATE usuario SET full_name=?, genre=?, nacionality=?, address=?, phone=? WHERE doc_number=? AND is_active=1',
+                [full_name, genre, nacionality, address, phone, data.row[0].doc_number],
+                (error) => {
+                    error ? res.status(500).json({ message: error.message }) : res.status(200).json({ message: "User updated successfully." })
                 })
-            }
         }
     })
 })
 
-router.delete('/delete_user/:doc_number', (req, res) => {
-    mysqlConnection.query(`UPDATE usuario SET is_active=0 WHERE doc_number = ${req.params.doc_number}`, (error, row) => {
-        error ? res.status(500).json({message: error.message}) : res.status(200).json({data: row})
+router.post('/change_password', token_required, (req, res) => {
+    jwt.verify(req.token, "3ywg&hsnxu43o9+iaz&sdtr", (error, data) => {
+        if (error) {
+            res.status(400).json({ message: error.message })
+        } else {
+            mysqlConnection.query(`SELECT * FROM usuario WHERE doc_number=${data.row[0].doc_number} AND is_active=1`, (error, row) => {
+                if (error) {
+                    res.status(500).json({ message: error.message })
+                } else {
+                    if (compareSync(req.body.new_password, row[0].password)) {
+                        res.status(400).json({ message: 'La nueva contraseña es igual a la anterior' })
+                    } else {
+                        const salt = genSaltSync(10);
+                        new_password = hashSync(req.body.new_password, salt);
+                        mysqlConnection.query('UPDATE usuario SET password=? WHERE doc_number=?', [new_password, data.row[0].doc_number],
+                            (error, row) => {
+                                error ? res.status(500).json({ message: error.message }) : res.status(200).json({ message: "Your password was updated successfully", data: row })
+                            })
+                    }
+                }
+            })
+        }
+    })
+})
+
+router.delete('/delete_user', token_required, (req, res) => {
+    jwt.verify(req.token, "3ywg&hsnxu43o9+iaz&sdtr", (error, data) => {
+        if (error) {
+            res.status(400).json({ message: error.message })
+        } else {
+            mysqlConnection.query(`UPDATE usuario SET is_active=0 WHERE doc_number = ${data.row[0].doc_number}`, (error, row) => {
+                error ? res.status(500).json({ message: error.message }) : res.status(200).json({ message: "Your account was deleted successfully." })
+            })
+        }
     })
 })
 
 router.post('/login', (req, res) => {
-    console.log(req.body.doc_number)
     mysqlConnection.query(`SELECT * FROM usuario WHERE doc_number = ${req.body.doc_number}`, (error, row) => {
         if (row.length >= 1) {
-            compareSync(req.body.password, row[0].password)? res.status(200).json({token: jwt.sign({row}, "3ywg&hsnxu43o9+iaz&sdtr")}) : res.status(400).json({message: 'La contraseña es incorrecta'})
+            compareSync(req.body.password, row[0].password) ? res.status(200).json({ token: jwt.sign({ row }, "3ywg&hsnxu43o9+iaz&sdtr", { expiresIn: "1h" }) }) : res.status(400).json({ message: 'La contraseña es incorrecta' })
         } else {
-            res.json({message: 'El usuario no está registrado'})
+            res.json({ message: 'El usuario no está registrado' })
         }
     })
 })
 
-function token_required(req, res, next) {
-    console.log(req.headers)
-    const bearer_header = req.headers.authorization
-    if (typeof bearer_header !== 'undefined') {
-        const bearer = bearer_header.split(' ')
-        console.log(bearer)
-        const token = bearer[1]
-        req.token = token
-        next()
-    } else {
-        res.status(403)
-    }
-}
 
 module.exports = router
