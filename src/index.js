d01b5fa646efc7656a927d4f2261967fcf8b0c8b
ManuelMarinDuque <Manuel.Duque5.2@gmail.com>
2021-04-16 00:05:13 -0500

    Websockets del crud de sedes.

--- src/index.js
+++ src/index.js
@@ -21,11 +21,11 @@ app.set("port", 3000);
 // Middlewares
 app.use(express.json());
 
-app.use((req, res, next) =>{
-  res.header('Access-Control-Allow-Origin','*'),
-  res.header('Access-Control-Allow-Headers','*'),
-  res.header('Access-Control-Allow-Methods','*'),
-  res.header('Allow','*');
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'),
+  res.header('Access-Control-Allow-Headers', '*'),
+  res.header('Access-Control-Allow-Methods', '*'),
+  res.header('Allow', '*');
   next();
 });
 
@@ -47,12 +47,42 @@ const server = app.listen(app.get("port"), () => {
 });
 
 // Websockets  
-const io = SocketIO(server)
+const io = SocketIO(server,{
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST","DELETE", "PUT"],
+    allowedHeaders: ["*"],
+    credentials: false
+  }
+});
 
 io.on('connection', (socket) => {
+
   socket.on('sede:listar_sedes', (data) => {
     mysqlConnection.query(`SELECT * FROM sede WHERE estado=1`, (error, row) => {
       error ? io.sockets.emit('sede:listar_sedes', { message: error.sqlMessage }) : io.sockets.emit('sede:listar_sedes', { data: row });
     })
   })
+
+  socket.on('sede:crear_sede', (data) => {
+    mysqlConnection.query('INSERT INTO sede SET ?', data, (error) => {
+      error ? io.sockets.emit('sede:crear_sede', { message: error.sqlMessage }) : io.sockets.emit('sede:crear_sede', { message: "Sede creada satisfactoriamente." })
+    })
+  })
+
+  socket.on('sede:editar_sede', (data) => {
+    const { nombre_sede, id_empresa, direccion, latitud, longitud, telefono, ciudad, id_sede } = data
+    mysqlConnection.query('UPDATE sede SET nombre_sede=?, id_empresa=?, direccion=?, latitud=?, longitud=?, telefono=?, ciudad=? WHERE id_sede=? AND estado=1',
+      [nombre_sede, id_empresa, direccion, latitud, longitud, telefono, ciudad, id_sede],
+      (error) => {
+        error ? io.sockets.emit('sede:editar_sede', { message: error.sqlMessage }) : io.sockets.emit('sede:editar_sede', { message: "Sede actualizada satisfactoriamente." })
+      })
+  })
+
+  socket.on('sede:eliminar_sede', (data) => {
+    mysqlConnection.query('INSERT INTO sede SET ?', data, (error) => {
+      error ? io.sockets.emit('sede:eliminar_sede', { message: error.sqlMessage }) : io.sockets.emit('sede:eliminar_sede', { message: "Sede eliminada satisfactoriamente." })
+    })
+  })
+
 })
--- src/routes/sede.routes.js
+++ src/routes/sede.routes.js
@@ -17,7 +17,7 @@ router.get('/obtener_sedes', token_required, (req, res) => {
             res.status(400).json({ message: error.message })
         } else {
             mysqlConnection.query(`SELECT * FROM sede WHERE estado=1`, (error, row) => {
-                error? res.status(500).json({ message: error.sqlMessage }) : res.status(200).json({data: row})
+                error ? res.status(500).json({ message: error.sqlMessage }) : res.status(200).json({ data: row })
             })
         }
     })
@@ -29,22 +29,22 @@ router.get('/obtener_sedes_empresa', token_required, (req, res) => {
             res.status(400).json({ message: error.message })
         } else {
             mysqlConnection.query(`SELECT * FROM sede WHERE id_empresa=${data.row[0].id_empresa} AND estado=1`, (error, row) => {
-                error? res.status(500).json({ message: error.sqlMessage }) : res.status(200).json({data: row})
+                error ? res.status(500).json({ message: error.sqlMessage }) : res.status(200).json({ data: row })
             })
         }
     })
 })
 
 router.get('/obtener_sede', token_required, (req, res) => {
-  jwt.verify(req.token, "3ywg&hsnxu43o9+iaz&sdtr", (error, data) => {
-      if (error) {
-          res.status(400).json({ message: error.message })
-      } else {
-          mysqlConnection.query(`SELECT * FROM sede WHERE id_sede=${data.row[0].id_sede} AND estado=1`, (error, row) => {
-              error? res.status(500).json({ message: error.sqlMessage }) : res.status(200).json({data: row})
-          })
-      }
-  })
+    jwt.verify(req.token, "3ywg&hsnxu43o9+iaz&sdtr", (error, data) => {
+        if (error) {
+            res.status(400).json({ message: error.message })
+        } else {
+            mysqlConnection.query(`SELECT * FROM sede WHERE id_sede=${data.row[0].id_sede} AND estado=1`, (error, row) => {
+                error ? res.status(500).json({ message: error.sqlMessage }) : res.status(200).json({ data: row })
+            })
+        }
+    })
 })
 
 router.put('/actualizar_sede', token_required, (req, res) => {
